96b621ef23dc0c638ba6bffb37c3de147733d56e
abhinavtripathy <abhinavtripathy99@gmail.com>
2020-01-25 16:11:12 -0500

    minor

--- back-end/api.py
+++ back-end/api.py
@@ -11,30 +11,15 @@ app = Flask(__name__)
 #API description
 apiDescription = [
     {
-        'title': u'Health Insights API',
-        'description': u'An API to query the healthcare dataset insights', 
-        'Author': u'DSC UMass'
+        'title': u'API',
+        'description': u'', 
+        'Author': u''
     }
 ]
 
-@app.route('/health-insights/api/v1.0/about', methods=['GET'])
+@app.route('/api', methods=['GET'])
 def get_tasks():
     return jsonify({'description': apiDescription[0]})
-
-@app.route('/health-insights/api/v1.0/<string:query>', methods=['GET'])
-def get_query(query):
-
-    with open('secret.json') as json_file:
-        secret = json.load(json_file)['secret']
-    
-    query_test = "b'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndlaXJkIn0.kjCG2vDIrrzFdF9HmGBJr8kFWl2p85xw2loFYZyyqa4'"
-    decoded = jwt.decode(query_test, secret, algorithms=['HS256'])
-
-    # except:
-    #     return "error"
-    
-        
-    return jsonify({'response': decoded})
 
 
 # Helper Functions 
