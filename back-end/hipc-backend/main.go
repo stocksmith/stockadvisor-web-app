f460c5591390acc436ce77abfd73cc52bb80e1c5
abhinavtripathy <abhinavtripathy99@gmail.com>
2020-01-30 16:08:34 -0500

    is that the case

--- back-end/hipc-backend/main.go
+++ back-end/hipc-backend/main.go
@@ -13,7 +13,9 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	fmt.Println("server has started on port 8080")
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
 	log.Fatal(http.ListenAndServe(":8080", router))
+
 }
